d1490d104e8f8d3bfc58c4f9ea397d2cf9e65818
Jackson Gold <59925511+jacksongold@users.noreply.github.com>
2020-01-15 08:18:57 -0800

    completed up to formatting strings

--- python_strings.py
+++ python_strings.py
@@ -8,9 +8,10 @@
 #       -set this equal to your birth year (doesn't have to be real should be less then 100 yrs ago)
 #   - current_year
 #       -set this equal to 2020
-
-
-
+my_first_name = "Jackson" #python var assignment does not require a keyword
+my_last_name = "Gold"
+my_year_of_birth = 1997
+current_year = 2020
 
 # TODO String Indexing
 #   - Print the following items (one per line) (print using variables)
@@ -20,25 +21,29 @@
 #       - second letter of your last name (use the -index)
 #       - first two letter of your first name (use the +index)
 #       - second two letter of your last name (use the -index)
-
-
-
+print (my_first_name)
+print (my_last_name) 
+print (my_year_of_birth)
+print (my_first_name[0])
+print (my_last_name[-1])
+print ( my_first_name [:2])
+print (my_last_name[-2:])
 
 #TODO Combining Strings
 #   - Print the following items (one per line) (print using variables)
 #       -first name and last name combined
 #       -first name six times
-
-
-
-
+print (my_first_name + my_last_name)
+print (my_first_name * 6)
+print (my_first_name, my_last_name)
 
 # TODO Formatting Strings
 #   - Print the following items (one per line) (print using variables)
 #       - first name last name -was born in- year of birth
 #       - first name last name -was born in- year of birth. first name -enjoyed celebrating- current year
-
-
+txt = my_first_name + my_last_name + ' was born in {}'
+print (txt.format (my year of birth))
+txt2 = my_first_name
 
 # TODO Escape characters
 #   - Print the following items (one per line) (print using variables)
